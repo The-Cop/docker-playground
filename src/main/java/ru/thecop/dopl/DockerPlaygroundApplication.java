12c335497d63a0107ff6bcd902a076d4d3407d0a
Kapkov Kirill <kap-kirill@yandex.ru>
2018-03-31 12:22:04 +0300

    initial commit

---
+++ src/main/java/ru/thecop/dopl/DockerPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package ru.thecop.dopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPlaygroundApplication.class, args);
+	}
+}
